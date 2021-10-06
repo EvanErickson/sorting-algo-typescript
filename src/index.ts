93354684b8d5f7cdb63f1c6934add162d88a3af5
Evan Erickson <evanserickson@gmail.com>
2021-10-06 15:35:39 -0700

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
+// const numbersCollection = new NumbersCollection([10,3000,-5,0])
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
 
-const sorter = new Sorter([10, 3, -5, 0]);
+// const charactersCollection = new CharactersCollection('adXawyb')
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3); 
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(sorter.collection);
+linkedList.print();
